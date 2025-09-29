c6f5f60044020a8fa31938ed7c45e7532418cec3
Deniss <cvetkovsdeniss4@gmail.com>
2025-09-29 10:27:53 +0300

    Added form.php file

---
+++ resources/views/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    <head>
+    <body>
+        <form method="post">
+            <label for="name">Name</label>
+            <input name="name" type="text">
+            <br>
+            <!---->
+            <label for="email">Email</label>
+            <input name="email" type="text">
+            <br>
+            <!---->
+            <input type="submit">
+        </form>
+    </body>
+</html>
